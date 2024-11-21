0129ce7ae105ca836557a138f6756a9be0a53e55
nirmal kumar <nirmal.rout193@hotmail.com>
2024-11-21 17:58:46 +0530

    changed to map

--- src/main/java/org/example/snake_N_ladders/Board.java
+++ src/main/java/org/example/snake_N_ladders/Board.java
@@ -3,30 +3,31 @@ package org.example.snake_N_ladders;
 import org.example.snake_N_ladders.Obstacles.Obstacle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Board {
     private int size;
-    private List<Obstacle> obstacleList;
+    private Map<Integer,Obstacle> obstacleMap;
 
     public Board(int size){
         this.size = size;
-        obstacleList = new ArrayList<>();
+        obstacleMap = new HashMap<>();
     }
     public int getSize(){
         return size;
     }
     public void addObstacle(Obstacle obstacle){
-        obstacleList.add(obstacle);
+        obstacleMap.putIfAbsent(obstacle.getStart(),obstacle);
     }
     public int processPosition(int position){
-        for(Obstacle obstacle : obstacleList){
-            if(position == obstacle.getStart()){
-                System.out.println("Hit an obstacle " + obstacle.getType()
-                        + "from start " + obstacle.getStart()  + "to end " + obstacle.getEnd());
-                return obstacle.getEnd();
+            if(obstacleMap.containsKey(position)){
+                Obstacle obs = obstacleMap.get(position);
+                System.out.println("Hit an obstacle " + obs.getType()
+                        + "from start " + obs.getStart()  + "to end " + obs.getEnd());
+                return obs.getEnd();
             }
-        }
         return position;
     }
 }
